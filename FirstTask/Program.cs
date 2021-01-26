884ee56ebd631c4be1284e412bf29ef45b12bec2
DESKTOP-RAQQDTT\User <aizak30042002@gmail.com>
2021-01-26 13:29:31 +0300

    Fifth Lab

---
+++ FirstTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTask
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool IsarraySizeANumber = int.TryParse(Console.ReadLine(),out int arraySize);
+            if (IsarraySizeANumber)
+            {
+                float[] initialArray = new float[arraySize];
+                for (int i = 0; i < arraySize; i++)
+                {
+                    Console.WriteLine($"Enter {i+1} element");
+                    bool IsElementANumber = float.TryParse(Console.ReadLine(), out float element);
+                    if (IsElementANumber)
+                        initialArray[i] = element;
+                    else
+                    {
+                        Console.WriteLine("It's not a float number.Try again");
+                        i--;
+                    }
+                }
+            }
+            
+        }
+       
+        
+    }
+}
